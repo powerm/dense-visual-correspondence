478c34ed1e2340cb817aac06afdafe36aef0b306
johnny <powermwhl@gmail.com>
2021-07-12 19:16:25 +0800

    init: add new file

---
+++ data_process/utils.py
@@ -0,0 +1,131 @@
+import sys
+import os
+import time
+import yaml
+import numpy as np
+from yaml import CLoader
+import datetime
+sys.path.append('..')
+import  transformations
+
+
+def getSpartanSourceDir():
+    return os.getenv("SPARTAN_SOURCE_DIR")
+
+def get_data_dir():
+    return os.getenv("DATA_DIR")
+
+def getQuaternionFromDict(d):
+    quat = None
+    quatNames = ['orientation', 'rotation', 'quaternion']
+    for name in quatNames:
+        if name in d:
+            quat = d[name]
+
+
+    if quat is None:
+        raise ValueError("Error when trying to extract quaternion from dict, your dict doesn't contain a key in ['orientation', 'rotation', 'quaternion']")
+
+    return quat
+
+def compute_angle_between_quaternions(q, r):
+    """
+    Computes the angle between two quaternions.
+
+    theta = arccos(2 * <q1, q2>^2 - 1)
+
+    See https://math.stackexchange.com/questions/90081/quaternion-distance
+    :param q: numpy array in form [w,x,y,z]. As long as both q,r are consistent it doesn't matter
+    :type q:
+    :param r:
+    :type r:
+    :return: angle between the quaternions, in radians
+    :rtype:
+    """
+
+    theta = 2*np.arccos(2 * np.dot(q,r)**2 - 1)
+    return theta
+
+def dictFromPosQuat(pos, quat):
+    d = dict()
+    d['translation'] = dict()
+    d['translation']['x'] = pos[0]
+    d['translation']['y'] = pos[1]
+    d['translation']['z'] = pos[2]
+
+    d['quaternion'] = dict()
+    d['quaternion']['w'] = quat[0]
+    d['quaternion']['x'] = quat[1]
+    d['quaternion']['y'] = quat[2]
+    d['quaternion']['z'] = quat[3]
+
+    return d
+
+def get_current_YYYY_MM_DD_hh_mm_ss():
+    """
+    Returns a string identifying the current:
+    - year, month, day, hour, minute, second
+
+    Using this format:
+
+    YYYY-MM-DD-hh-mm-ss
+
+    For example:
+
+    2018-04-07-19-02-50
+
+    Note: this function will always return strings of the same length.
+
+    :return: current time formatted as a string
+    :rtype: string
+
+    """
+
+    now = datetime.datetime.now()
+    string =  "%0.4d-%0.2d-%0.2d-%0.2d-%0.2d-%0.2d" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    return string
+
+def getDictFromYamlFilename(filename):
+    """
+    Read data from a YAML files
+    """
+    with open(filename) as f:
+        return yaml.load(f.read(), Loader=CLoader)
+        #return yaml.load(f.read())
+
+def saveToYaml(data, filename):
+    with open(filename, 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+
+
+def homogenous_transform_from_dict(d):
+    """
+    Returns a transform from a standard encoding in dict format
+    :param d:
+    :return:
+    """
+    pos = [0]*3
+    pos[0] = d['translation']['x']
+    pos[1] = d['translation']['y']
+    pos[2] = d['translation']['z']
+
+    quatDict = getQuaternionFromDict(d)
+    quat = [0]*4
+    quat[0] = quatDict['w']
+    quat[1] = quatDict['x']
+    quat[2] = quatDict['y']
+    quat[3] = quatDict['z']
+
+    transform_matrix = transformations.quaternion_matrix(quat)
+    transform_matrix[0:3,3] = np.array(pos)
+
+    return transform_matrix
+
+
+if __name__ == '__main__':
+
+    filename = "/home/cyn/ws_moveit/src/fusion_server/config/poses.yaml"
+    #filename = "/home/cyn/ws_moveit/src/camera_config/config/kinectDK/master/camera_info.yaml"
+
+    di =getDictFromYamlFilename(filename)
+    print(di)
