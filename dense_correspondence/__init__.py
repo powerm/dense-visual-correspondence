7c7f93c1f4b8a0dc1d0ec5a82b7a4603c93d7fb6
johnny <powermwhl@gmail.com>
2021-07-12 20:28:14 +0800

    init:  __init__

---
+++ dense_correspondence/__init__.py
@@ -0,0 +1,6 @@
+#  borrow from  detectron2  https://github.com/facebookresearch/detectron2/blob/03c70397ace4f03d85e99ce0241f852f65d1eb16/detectron2/__init__.py
+from .utils.env import setup_environment
+
+setup_environment()
+
+__version__ = "0.0.1"
